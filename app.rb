eb88e4c34d3758d5f0dcf7534fbedfbe9a62b469
John Romkey <58883+romkey@users.noreply.github.com>
2019-05-27 17:15:22 -0700

    properly identify  SNMP responses
    only publish on change
    
    fix printer service arguments

--- app.rb
+++ app.rb
@@ -10,33 +10,42 @@ class PrinterHomeBusApp < HomeBusApp
 
     @manager_hostname = @options[:agent]
 
+    @old_status = ''
+    @old_total_page_count = 0
+
     super
   end
 
   def setup!
     @manager = SNMP::Manager.new(host: options[:agent], community: options[:community_string], version: :SNMPv1)
 
-    response = @manager.get(['sysDescr.0', 'sysName.0', 'sysLocation.0', 'sysUpTime.0', '1.3.6.1.2.1.1.5.0', '1.3.6.1.2.1.25.3.2.1.3.1'])
+    response = @manager.get(['sysDescr.0', 'sysName.0', 'sysLocation.0', 'sysUpTime.0', '1.3.6.1.2.1.43.5.1.1.17.1', '1.3.6.1.2.1.25.3.2.1.3.1'])
     response.each_varbind do |vb|
       @sysName = vb.value.to_s if  vb.name.to_s == 'SNMPv2-MIB::sysName.0'
       @sysDescr = vb.value.to_s if  vb.name.to_s == 'SNMPv2-MIB::sysDescr.0'
       @sysLocation = vb.value.to_s if  vb.name.to_s == 'SNMPv2-MIB::sysLocation.0'
-      @serial_number = vb.value.to_s if vb.name.to_s == '1.3.6.1.2.1.1.5.0'
-      @model = vb.value.to_s if vb.name.to_s == '1.3.6.1.2.1.25.3.2.1.3.1'
+      @serial_number = vb.value.to_s if vb.name.to_s == 'SNMPv2-SMI::mib-2.43.5.1.1.17.1'
+      @model = vb.value.to_s if vb.name.to_s == 'SNMPv2-SMI::mib-2.25.3.2.1.3.1'
     end
 
-    puts @sysName, @sysDescr, @sysLocation, @model, @serial_number
+    if(@options[:verbose])
+      puts @sysName, @sysDescr, @sysLocation, @model, @serial_number
+    end
 
   end
 
   def work!
+    status = ''
+    total_page_count = 0
+    remaining_belt_unit_pages = 0
+    remaining_drum_unit_pages = 0
+
     response = @manager.get([ '1.3.6.1.2.1.43.11.1.1.9.1.6', '1.3.6.1.2.1.43.11.1.1.9.1.7', '1.3.6.1.2.1.43.16.5.1.2.1.1', '1.3.6.1.2.1.43.10.2.1.4.1.1' ])
     response.each_varbind do |vb|
-      remaining_belt_unit_pages = vb.value.to_i if vb.name.to_s == '1.3.6.1.2.1.43.11.1.1.9.1.6'
-      remaining_drum_unit_pages = vb.value.to_i if vb.name.to_s == '1.3.6.1.2.1.43.11.1.1.9.1.7'
-      status  = vb.value.to_s if vb.name.to_s == '1.3.6.1.2.1.43.16.5.1.2.1.1'
-      total_page_count = vb.value.to_i if vb.name.to_s == '1.3.6.1.2.1.43.10.2.1.4.1.1'
-      pp vb
+      remaining_belt_unit_pages = vb.value.to_i if vb.name.to_s == 'SNMPv2-SMI::mib-2.43.11.1.1.9.1.6'
+      remaining_drum_unit_pages = vb.value.to_i if vb.name.to_s == 'SNMPv2-SMI::mib-2.43.11.1.1.9.1.7'
+      status  = vb.value.to_s if vb.name.to_s == 'SNMPv2-SMI::mib-2.43.16.5.1.2.1.1'
+      total_page_count = vb.value.to_i if vb.name.to_s == 'SNMPv2-SMI::mib-2.43.10.2.1.4.1.1'
     end
 
 
@@ -44,29 +53,29 @@ class PrinterHomeBusApp < HomeBusApp
       pp results
     end
 
-    timestamp = Time.now.to_i
+    if(status != @old_status || total_page_count != @old_total_page_count)
+      timestamp = Time.now.to_i
 
-    # only publish if changed
-
-    @mqtt.publish '/printer',
-                  JSON.generate({ id: @uuid,
-                                  timestamp: timestamp,
-                                  model: @model,
-                                  serial_number: @serial_number,
-                                  status: status,
-                                  total_page_count: total_page_count,
-                                  resources: [
-                                    {
-                                      name: 'remaining_belt_unit_pages',
-                                      count: remaining_belt_unit_pages
-                                    },
-                                    {
-                                      name: 'remaining_drum_unit_pages',
-                                      count: remaining_drum_unit_pages
-                                    }
-                                  ]
-                                }),
-                  true
+      @mqtt.publish '/printer',
+                    JSON.generate({ id: @uuid,
+                                    timestamp: timestamp,
+                                    model: @model,
+                                    serial_number: @serial_number,
+                                    status: status,
+                                    total_page_count: total_page_count,
+                                    resources: [
+                                      {
+                                        name: 'remaining_belt_unit_pages',
+                                        count: remaining_belt_unit_pages
+                                      },
+                                      {
+                                        name: 'remaining_drum_unit_pages',
+                                        count: remaining_drum_unit_pages
+                                      }
+                                    ]
+                                  }),
+                    true
+    end
 
     sleep 60
   end
